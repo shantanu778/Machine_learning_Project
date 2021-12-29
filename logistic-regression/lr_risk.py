00294ef254ab85d81ce478a40358d891ab2d0a4d
NoaSchwensfeier <n.schwensfeier@googlemail.com>
2021-12-29 11:38:14 -0800

    fixed relative paths

--- logistic-regression/lr_risk.py
+++ logistic-regression/lr_risk.py
@@ -7,11 +7,11 @@ results = pd.read_csv('lr_optimal.csv', sep=',')
 #print(results)
 
 # Load train data to verify test error on train set. 
-X_train = np.load('/Users/noaschwensfeier/python_ml/Machine_learning_Project/dataset/X_train.npy')
-y_train = np.load('/Users/noaschwensfeier/python_ml/Machine_learning_Project/dataset/y_train.npy')
-classifier.score(X_train, y_train)
+X_train = np.load('../dataset/X_train.npy')
+y_train = np.load('../dataset/y_train.npy')
+print(classifier.score(X_train, y_train))
 
 # Load test data to estimate risk. 
-X_test = np.load('/Users/noaschwensfeier/python_ml/Machine_learning_Project/dataset/X_test.npy')
-y_test = np.load('/Users/noaschwensfeier/python_ml/Machine_learning_Project/dataset/y_test.npy')
-classifier.score(X_test, y_test)
+X_test = np.load('../dataset/X_test.npy')
+y_test = np.load('../dataset/y_test.npy')
+print(classifier.score(X_test, y_test))
