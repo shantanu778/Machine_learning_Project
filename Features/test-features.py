0674b63bcf1c6cc80ceb1c7c05a117999a3970cf
rowan1697 <rowan.hossain@gmail.com>
2022-01-10 13:40:53 +0100

    modified features

--- Features/test-features.py
+++ Features/test-features.py
@@ -3,76 +3,73 @@ from sklearn import svm
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from feature_extraction import structure, hog, lbp, gradient, hotspots 
 from sklearn.pipeline import Pipeline
+from feature_extraction import FeatureExtractor
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
 
 
+# training set
+X_train = np.load('../dataset/X_train.npy')
+X_train = np.array(X_train, dtype=np.int32)
+y_train = np.load('../dataset/y_train.npy')
 
-with open('../dataset/dataset.txt','r') as data:
-    X = [line.strip().split("  ") for line in data]
-    y = [i for i in range(10) for j in range(200)]
-X = np.array(X, dtype=np.int32)
+# testing set
+X_test = np.load('../dataset/X_test.npy')
+X_test = np.array(X_test, dtype=np.int32)
+y_test = np.load('../dataset/y_test.npy')
 
-
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.50,random_state=43)
-
-
-#PCA
 scaler = StandardScaler()
-pca = PCA(n_components=100,svd_solver='auto')
-classifier = svm.SVC(kernel='rbf')
-pipe = Pipeline(steps=[('scaler', scaler), ('pca', pca), ('classifier', classifier)])
-pipe.fit(X_train,y_train)
-print("PCA: {}".format(pipe.score(X_test, y_test,)))
+pca = PCA(svd_solver='auto')
+classifier = svm.SVC()
+pipe = Pipeline(steps=[('scaler', 'passthrough'), ('pca', 'passthrough'), ('classifier', classifier)])
+
+extr_methods = ['structure', 'hog', 'gradient', 'hotspots', 'lbp']
+
+for extr_method in extr_methods:
 
 
+    ft_structure = FeatureExtractor(method=extr_method)
+    X_train_transf = ft_structure.transform(X_train, cell_size=2)
+    X_test_transf = ft_structure.transform(X_test, cell_size=2)
 
-X_train = X_train.reshape(1000,16,15)
-X_test = X_test.reshape(1000,16,15)
+    # Specify parameter space to search.
+    param_grid = [
+        {'scaler': [None],
+        'pca': [None], 
+        'classifier__C': np.logspace(-4, 2, num=10),
+        'classifier__kernel': ['linear', 'rbf', 'poly']
+        },
 
-#Structure
-features_train = structure(X_train,cell_size=2)
-features_test = structure(X_test,cell_size=2)
-clf = svm.SVC(kernel='rbf').fit(features_train, y_train)
-print("Structure: {}".format(clf.score(features_test, y_test,)))
+        {'scaler': [scaler],
+        'pca': [None], 
+        'classifier__C': np.logspace(-4, 2, num=10),
+        'classifier__kernel': ['linear', 'rbf', 'poly']},
 
+        {'scaler': [None],
+        'pca': [pca],
+        'pca__n_components': [i for i in range(2, X_train_transf.shape[1]+1)], 
+        'classifier__C': np.logspace(-4, 2, num=10),
+        'classifier__kernel': ['linear', 'rbf', 'poly']
+        },
 
+        {'scaler': [scaler],
+        'pca': [pca],
+        'pca__n_components': np.linspace(2, X_train_transf.shape[1], X_train_transf.shape[1]-1, dtype=int), 
+        'classifier__C': np.logspace(-4, 2, num=10),
+        'classifier__kernel': ['linear', 'rbf', 'poly']
+        }
+        ]
 
+    # Initialise the GridSearchCV object and run the grid search. 
+    search = GridSearchCV(pipe, param_grid=param_grid, n_jobs=8, cv=5)
+    search.fit(X_train_transf, y_train)
 
+    # Print test accuracy and parameters. Note: this refers to the highest
+    # mean accuracy on the test datasets during k-fold cross-validation.
+    print("=====================================================")
+    print(f'\n{extr_method}')
+    print('Best parameter (CV score=%0.3f):' % search.best_score_)
+    print(search.best_params_)
 
-#HOG 
-features_train = hog(X_train)
-features_test = hog(X_test)
-pipe.fit(features_train,y_train)
-print("HOG: {}".format(pipe.score(features_test, y_test,)))
-
-
-#Gradient
-features_train = gradient(X_train)
-features_test = gradient(X_test)
-pipe.fit(features_train,y_train)
-print("Gradient: {}".format(pipe.score(features_test, y_test,)))
-
-
-
-
-#Hotspots
-features_train = hotspots(X_train)
-features_test = hotspots(X_test)
-clf = svm.SVC(kernel='rbf').fit(features_train, y_train)
-clf.score(features_test, y_test,)
-print("Hotspots: {}".format(clf.score(features_test, y_test,)))
-
-
-
-
-#LBP
-features_train = lbp(X_train)
-features_test = lbp(X_test)
-clf = svm.SVC(kernel='rbf').fit(features_train, y_train)
-clf.score(features_test, y_test,)
-print("LBP: {}".format(clf.score(features_test, y_test,)))
-
-
-
-
+    # Print training accuracy of optimal model, trained on the entire train set. 
+    print(search.best_estimator_.score(X_test_transf, y_test))
