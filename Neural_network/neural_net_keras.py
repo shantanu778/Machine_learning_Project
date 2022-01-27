d1ab7f574deddf9e68dc33f38fe22c44da456f66
shantanu778 <shantunath778@gmail.com>
2022-01-27 20:45:33 +0100

    added cross-validation, gridsearchCV

--- Neural_network/neural_net_keras.py
+++ Neural_network/neural_net_keras.py
@@ -1,8 +1,9 @@
+from gc import callbacks
+from time import time
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 # training set
 X_train = np.load('../dataset/X_train.npy')
 X_train = np.array(X_train, dtype=np.int32)
@@ -30,24 +31,54 @@ def preprocess_data(x, y):
 
 
 X_train, y_train = preprocess_data(X_train, y_train)
-X_test, y_test = preprocess_data(X_test, y_test)
+#X_test, y_test = preprocess_data(X_test, y_test)
+
+
+# Keras model
+def create_model(nodes=8,lr_rate = 0.001, activation = None, dropout=0.0):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape = (240,1)), 
+        tf.keras.layers.Dense(nodes, activation = activation),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(nodes, activation = activation),
+        tf.keras.layers.Dense(10, activation = 'softmax')
+    ])
+    model.compile(optimizer = tf.keras.optimizers.SGD(learning_rate = lr_rate), loss='categorical_crossentropy', metrics = ['accuracy'])
+    return model
 
 
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape = (240,1)), 
-    tf.keras.layers.Dense(32, activation = 'tanh'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(32, activation = 'tanh'),
-    tf.keras.layers.Dense(10, activation = 'softmax')
-])
+start=time()
+# Parameters
+nodes = [32, 64, 16]
+activation = ['tanh', 'relu', 'sigmoid']
+lr_rate = [0.005, 0.01, 0.001]
+dropout = [0.2, 0.3, 0.4]
+model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=create_model, epochs=200)
 
-model.compile(optimizer = 'sgd', loss='categorical_crossentropy', metrics = ['accuracy'])
+param_grid = dict(nodes=nodes, lr_rate=lr_rate, activation=activation, dropout=dropout)
 
-model.fit(X_train, y_train, epochs = 100)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, refit = True, cv=5, verbose=2)
+
+# Early Stopping
+es = tf.keras.callbacks.EarlyStopping(
+    monitor='loss', patience=10,
+    mode='min', restore_best_weights=True
+)
+
+grid_result = grid.fit(X_train, y_train ,verbose=0, callbacks=[es])
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+print("total time:",time()-start)
 
 
-model.evaluate(X_test, y_test)
+'''
+grid_predictions = grid_result.predict(X_test.reshape(X_test.shape[0], 240,1)) 
+print(classification_report(y_test, grid_predictions)) 
+'''
 
+model = create_model(grid_result.best_params_['nodes'],grid_result.best_params_['lr_rate'],grid_result.best_params_['activation'], grid_result.best_params_['dropout'])
+model.fit(X_train, y_train, epochs = 200, batch_size=None)
+y_pred= model.predict(X_test.reshape(X_test.shape[0], 240,1)) 
+print(classification_report(y_test, np.argmax(y_pred, axis=1))) 
 
 
